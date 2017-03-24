4497323c83c404498b43dd0a67451118c260f795
Joan Pau <joanpau.castells@hotmail.com>
2017-03-24 18:36:48 +0100

    Models v1

--- iVendaDePelis/models.py
+++ iVendaDePelis/models.py
@@ -1,28 +1,34 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-def Film(models.Model):
-    film_id = models.IntegerField()
-    title = models.TextField()
-    year = models.DateField()
-    duration = models.IntegerField()
-    genre = models.TextField()
-    plot = model.TextField()
-    price = models.DecimalField('€', max_digits = 2, decimal_places = 2)
 
-def Actor(models.Model):
-    name = models.TextField()
-    birthday = models.DateField()
-    country = models.TextField()
-    biography = models.TextField()
+class Film(models.Model):
+    title = models.TextField(max_length=20)
+    year = models.DateField(max_length=20)
+    duration = models.IntegerField(max_length=3)
+    genre = models.TextField(max_length=20)
+    plot = models.TextField()
+    price = models.DecimalField('$', max_digits = 4, decimal_places = 2,default=18)
+    client=models.ManyToManyField(User)
 
-def Review(models.Model):
+class Actor(models.Model):
+    name = models.TextField(max_length=30)
+    birthday = models.DateField("birthday")
+    country = models.TextField(blank=True,null=True)
+    biography = models.TextField(blank=True,null=True)
+    films=models.ManyToManyField(Film)
+
+
+class Review(models.Model):
     RATING_CHOICES = ((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'),\
     (5, 'five'))
-    review_id = models.IntegerField()
     rating = models.PositiveSmallIntegerField('Rating (stars)', blank = False, default = 3, choices = RATING_CHOICES)
     opinion = models.TextField()
+    user=models.ForeignKey(User)
+    film=models.ForeignKey(Film)
+class FavouriteList(models.Model):
+    user=models.ForeignKey(User)
+    films=models.ManyToManyField(Film)
 
-def Favourite_list(models.Model):
-    
